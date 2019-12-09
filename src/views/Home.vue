6fbe35e363458becd2578d63cb420c17d8a03490
unknown <weihaiquan@bianfeng.com>
2019-12-09 16:42:56 +0800

    初步框架样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,8 @@
 <template>
     <div id="home">
-        <!-- <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-        </el-breadcrumb> -->
-        <el-menu :default-active="activeIndex" 
-        class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="1">处理中心</el-menu-item>
+        <!-- <el-menu default-active="/home/processing" 
+            class="el-menu-demo" mode="horizontal"  :router="true">
+            <el-menu-item index="/home/processing">处理中心</el-menu-item>
             <el-submenu index="2">
                 <template slot="title">我的工作台</template>
                 <el-menu-item index="2-1">选项1</el-menu-item>
@@ -23,22 +17,32 @@
             </el-submenu>
             <el-menu-item index="3" disabled>消息中心</el-menu-item>
             <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-        </el-menu>
-
-            首页大屏
-        
+        </el-menu> -->
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+            <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        </el-tabs>
+        <router-view/>        
     </div>
 </template>
 <script>
 export default {
     data(){
         return {
-            activeIndex:"1"
+            // activeIndex:"/processing"
+            activeName:"first"
         }
     },
     methods:{
-        handleSelect(key, keyPath){
-            window.console.log(key, keyPath);
+        // handleSelect(key, keyPath){
+        //     window.console.log("key:",key, "keyPath:",keyPath[0]);
+        //     this.$router.push({path: keyPath[0]})
+        // }
+        handleClick(tab,event){
+            window.console.log(tab,event)
+            //在vue-router的js文件中解决同一路由跳转
+            this.$router.push({path:"/home/"+tab.paneName})
         }
     }
 }
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="home">
     <div class="container">
-      <div class="left-nav">
-        <div class="title"> <i class="el-icon-upload logo"></i> 服务平台</div>
+      <div :class="isCollapse==false?'left-nav':['left-nav','left-nav--collapse']">
+        <div :class="isCollapse==false?['left-nav__title']:['left-nav__title--collapse']"> 
+          <i :class="isCollapse==false?['el-icon-upload', 'left-nav__logo']:['el-icon-upload','left-nav__logo--collapse']"></i> 服务平台</div>
         <div id="nav" >
           <el-menu 
             :collapse="isCollapse"
             background-color="#fff"
             text-color="#218868"
             active-text-color="teal"
+            :collapse-transition="false"
             :router="true"
             default-active="/home" class="el-menu-vertical-demo" >
             <el-menu-item index="/home">
@@ -34,13 +36,13 @@
           </el-menu>
         </div>
       </div>
-      <div class="right-content">
-        <div class="right-header">
-          <el-row>
-            <el-col :span="2">
+      <div :class="isCollapse==false?'right-content':['right-content','right-content--collapse']">
+        <div :class="isCollapse==false?'right-header':['right-header','right-header--collapse']">
+          <el-row type="flex" justify="start">
+            <el-col :span="1">
               <i class="el-icon-s-fold" @click="canCollapse"></i>
             </el-col>
-            <el-col :span="15">
+            <el-col :span="18">
               <div>
               <el-input placeholder="请输入内容"  class="input-with-select">
                 <el-select v-model="select" slot="prepend" placeholder="请选择">
@@ -52,11 +54,13 @@
               </el-input>
             </div>
             </el-col>
-            <el-col :offset="2" :span="1">
+            <el-col :offset="1" :span="1">
                 <el-dropdown  @command="handleCommand">
-                  <span class="el-dropdown-link">
-                      <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-                  </span>
+                  <el-badge :value="3" class="bage--news">
+                    <span class="el-dropdown-link">
+                        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+                    </span>
+                  </el-badge>
                   <el-dropdown-menu slot="dropdown">
                     <el-dropdown-item icon="el-icon-plus" command="update">修改信息</el-dropdown-item>
                     <el-dropdown-item icon="el-icon-circle-check" command="logout">退出登陆</el-dropdown-item>
@@ -65,7 +69,7 @@
             </el-col>
           </el-row>
         </div>
-        <div class="content">
+        <div :class="isCollapse==false?'content':['content','content--collapse']">
             <router-view/>
         </div>
       </div>
@@ -99,7 +103,10 @@ export default {
 }
 .container {
   height: 100%;
-  /* display: flex; */
+  width: 100%;
+  /* 去除水平滚动条 */
+  overflow: auto;
+  box-sizing: border-box;
 }
 .left-nav {
   width: 200px; 
@@ -110,32 +117,45 @@ export default {
   height: 100%;
   overflow-x: hidden;
 }
-.container > .left-nav .title {
+.container > .left-nav--collapse {
+  width: 64px; 
+}
+.left-nav__title {
   line-height: 2.5em;
   font-size: 18px;
   text-align: left;
   color: #333;
   font-weight: bold;
 }
+.left-nav__title--collapse {
+  font-size: 10px;
+  text-align: left;
+  color: #333;
+}
 .el-menu-item.is-active {
   background-color: #FCFCFC !important;
   border:1px solid teal;
 }
-.logo {
+.left-nav__logo {
   color: teal;
   font-size: 22px;
 }
+.left-nav__logo--collapse {
+  color: teal;
+  font-size: 12px;
+}
 #nav {
   margin-right: -1px;
-}
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
 }
 .container > .right-content {
   /* flex:1; */
   background-color: #f1f2f7;
   height: 100%;
+  margin-left:201px;
+  width: 100%;
+}
+.container > .right-content--collapse {
+  margin-left:65px;
 }
 .container > .right-content > .right-header {
   padding: 0 1em;
@@ -145,6 +165,16 @@ export default {
   position: fixed;
   width: 100%;
   left:201px;
+}
+.container > .right-content > .right-header--collapse {
+  left:65px;
+}
+.bage--news {
+  margin-top: 6px;
+  margin-right: 40px;
+}
+.container  .el-select {
+  width: 100px;
 }
 .content {
   background-color: #ffffff;
@@ -156,5 +186,8 @@ export default {
   left:201px;
   right: 0;
   bottom: 0;
+}
+.content--collapse {
+  left:65px;
 }
 </style>
