108e3fc790ec087362ce16586ba4a96950037824
unknown <weihaiquan@bianfeng.com>
2019-12-20 17:14:02 +0800

    table二次封装

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    {{ checkList }}
     <el-table
       ref="multipleTable"
       :data="table.tableData"
       style="width: 100%"
-      @selection-change="handleSelectionChange"
+      @selection-change="handleSelectionChange(table)"
     >
       <el-table-column
         v-for="(item, index) in table.header"
@@ -19,7 +18,7 @@
           <slot v-if="item.prop == 'option'" name="table" v-bind:record="props.row"></slot>
           <span v-else-if="item.prop == 'selection'">
             <el-checkbox-group v-model="checkList">
-              <el-checkbox :value="props.row.id" :label="props.row.id"></el-checkbox>
+              <el-checkbox :value="props.row.id"></el-checkbox>
             </el-checkbox-group>
           </span>
           <span v-else>{{ props.row[item.prop] }}</span>
@@ -47,7 +46,19 @@ export default {
     table: { type: Object }
   },
   methods: {
-    handleSelectionChange() {}
+    handleSelectionChange(table) {
+      var len = table.tableData.length;
+      window.console.log(len, "==len===");
+      window.console.log(this.checkList.length, "==this.checkList.length===");
+
+      if (this.checkList.length !== len) {
+        this.checkList = table.tableData.map(item => item.id);
+      } else {
+        this.checkList = [];
+      }
+      // this.checkList = table.tableData.map(item => item.id);
+      window.console.log(this.checkList, "=====");
+    }
   },
   mounted() {
     window.console.log(this.table.header);
